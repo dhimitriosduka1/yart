203764eff8bfc4b706a41f65e5438600284b0e76
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2023-05-19 20:27:04 +0200

    Added progress indicator

--- src/main.py
+++ src/main.py
@@ -1,24 +1,13 @@
-# This is a sample Python script.
 from src.utils import write_ppm
+from tqdm import tqdm
 
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     columns = 256
     rows = 256
 
     data = []
 
-    for i in range(rows - 1, 0, -1):
+    for i in tqdm(range(rows - 1, 0, -1)):
         for j in range(columns):
             r = j / (columns - 1)
             g = i / (rows - 1)
@@ -31,5 +20,3 @@ if __name__ == '__main__':
             data.append([ir, ig, ib])
 
     write_ppm(data, rows, columns, "output.ppm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
