8c8bec33ca11287aa45560d919635b89be447f28
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2023-05-19 21:38:35 +0200

    Final implementation of vec3 and test class

--- src/vec3.py
+++ src/vec3.py
@@ -20,16 +20,50 @@ class Vec3:
     def length_squared(self):
         return self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
 
+    @staticmethod
+    def dot(u, v):
+        return u[0] * v[0] + u[1] * v[1] + u[2] * v[2]
+
+    @staticmethod
+    def cross(u, v):
+        return Vec3(
+            u[1] * v[2] - u[2] * v[1],
+            u[2] * v[0] - u[0] * v[2],
+            u[0] * v[1] - u[1] * v[0]
+        )
+
+    @staticmethod
+    def unit_vector(u):
+        return u / u.length()
+
     def __neg__(self):
         return Vec3(-self.e[0], -self.e[1], -self.e[2])
 
+    def __sub__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.e[0] - other[0], self.e[1] - other[1], self.e[2] - other[2])
+        self.e[0] -= other
+        self.e[1] -= other
+        self.e[2] -= other
+        return self
+
     def __add__(self, other):
-        self.e[0] += other[0]
-        self.e[1] += other[1]
-        self.e[2] += other[2]
+        if isinstance(other, Vec3):
+            return Vec3(self.e[0] + other[0], self.e[1] + other[1], self.e[2] + other[2])
+        self.e[0] += other
+        self.e[1] += other
+        self.e[2] += other
         return self
 
     def __mul__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self.e[0] * other[0], self.e[1] * other[1], self.e[2] * other[2])
+        self.e[0] *= other
+        self.e[1] *= other
+        self.e[2] *= other
+        return self
+
+    def __rmul__(self, other):
         self.e[0] *= other
         self.e[1] *= other
         self.e[2] *= other
@@ -40,3 +74,6 @@ class Vec3:
 
     def __getitem__(self, item):
         return self.e[item]
+
+    def __str__(self):
+        return f"{self.e[0]} {self.e[1]} {self.e[2]}"
--- test/test_vec3.py
+++ test/test_vec3.py
@@ -17,17 +17,47 @@ class TestVec3(TestCase):
         self.assertEqual(vec[1], -4.0)
         self.assertEqual(vec[2], -6.0)
 
-    def test_mul(self):
+    def test_sub_number(self):
+        vec = self.vec - self.t
+        self.assertEqual(vec[0], 0.0)
+        self.assertEqual(vec[1], 2.0)
+        self.assertEqual(vec[2], 4.0)
+
+    def test_sub_vec(self):
+        vec = self.vec - self.other_vec
+        self.assertEqual(vec[0], 1.0)
+        self.assertEqual(vec[1], 2.0)
+        self.assertEqual(vec[2], 3.0)
+
+    def test_mul_number_right(self):
         vec = self.vec * self.t
         self.assertEqual(vec[0], 4.0)
         self.assertEqual(vec[1], 8.0)
         self.assertEqual(vec[2], 12.0)
 
-    def test_add(self):
+    def test_mul_number_left(self):
+        vec = self.t * self.vec
+        self.assertEqual(vec[0], 4.0)
+        self.assertEqual(vec[1], 8.0)
+        self.assertEqual(vec[2], 12.0)
+
+    def test_mul_vec(self):
+        vec = self.vec * self.other_vec
+        self.assertEqual(vec[0], 2.0)
+        self.assertEqual(vec[1], 8.0)
+        self.assertEqual(vec[2], 18.0)
+
+    def test_add_vec(self):
         vec = self.vec + self.other_vec
         self.assertEqual(vec[0], 3.0)
         self.assertEqual(vec[1], 6.0)
         self.assertEqual(vec[2], 9.0)
+
+    def test_add_number(self):
+        vec = self.vec + self.t
+        self.assertEqual(vec[0], 4.0)
+        self.assertEqual(vec[1], 6.0)
+        self.assertEqual(vec[2], 8.0)
 
     def test_true_div(self):
         vec = self.vec / self.t
@@ -42,3 +72,19 @@ class TestVec3(TestCase):
     def test_length_squared(self):
         length_squared = self.vec.length_squared()
         self.assertEqual(length_squared, 56)
+
+    def test_dot(self):
+        dot = Vec3.dot(self.vec, self.other_vec)
+        self.assertEqual(dot, 28)
+
+    def test_cross(self):
+        cross = Vec3.cross(self.vec, self.other_vec)
+        self.assertEqual(cross[0], 0)
+        self.assertEqual(cross[1], 0)
+        self.assertEqual(cross[2], 0)
+
+    def test_unit_vector(self):
+        cross = Vec3.unit_vector(self.vec)
+        self.assertEqual(cross[0], 2.0 / sqrt(56))
+        self.assertEqual(cross[1], 4.0 / sqrt(56))
+        self.assertEqual(cross[2], 6.0 / sqrt(56))
