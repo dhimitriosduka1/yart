2bcb527173753cdd87c1b8cb5ce19cbe3f3ba983
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2023-05-20 22:54:51 +0200

    Added normals

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 from src.ray import Ray
 from src.vec3 import Vec3
 
@@ -21,12 +23,15 @@ def hit_sphere(center: Vec3, radius: float, ray: Ray):
     a = Vec3.dot(ray.direction, ray.direction)
     b = 2.0 * Vec3.dot(oc, ray.direction)
     c = Vec3.dot(oc, oc) - radius * radius
-    return (b * b - 4 * a * c) > 0
+    discriminant = (b * b - 4 * a * c)
+    return -1.0 if discriminant < 0 else ((-b - sqrt(discriminant)) / (2.0 * a))
 
 
 def ray_color(ray: Ray):
-    if hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, ray):
-        return Vec3(1, 0, 0)
+    t = hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, ray)
+    if t > 0.0:
+        normal: Vec3 = Vec3.unit_vector(ray.at(t) - Vec3(0.0, 0.0, -1.0))
+        return 0.5 * Vec3(normal.x() + 1, normal.y() + 1, normal.z() + 1)
     unit_direction: Vec3 = Vec3.unit_vector(ray.direction)
     t = 0.5 * (unit_direction.y() + 1.0)
     return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
